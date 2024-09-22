6bbe11a6a18070e48e530f69831a8d9b1f25a29f
dzifarr <dzifarennermensah@gmail.com>
2024-09-22 01:10:08 +0000

    added api integration

--- src/script.js
+++ src/script.js
@@ -1,10 +1,27 @@
+function openWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let weatherCityElement = document.querySelector("#weather-city");
+
+  weatherCityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "f7cf08ac67ob46f704t8ef4248a91135";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(openWeather);
+}
+
 function handleClick(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
-  let weatherCityElement = document.querySelector("#weather-city");
 
-  weatherCityElement.innerHTML = searchInputElement.value;
+  searchCity(searchInputElement.value);
 }
 
 let blueSearchElement = document.querySelector("#blue-search");
 blueSearchElement.addEventListener("submit", handleClick);
+
+searchCity("Accra");
